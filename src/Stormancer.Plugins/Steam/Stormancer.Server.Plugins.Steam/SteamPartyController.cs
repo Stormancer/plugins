cf155b0303956d7ab1a4a0020d9cbef05eabd289
Antoine Lafarge <antlafarge@hotmail.com>
2020-03-26 13:15:32 +0100

    -

--- src/Stormancer.Plugins/Steam/Stormancer.Server.Plugins.Steam/SteamPartyController.cs
+++ src/Stormancer.Plugins/Steam/Stormancer.Server.Plugins.Steam/SteamPartyController.cs
@@ -21,56 +21,61 @@
 // SOFTWARE.
 
 using Stormancer.Server.Plugins.API;
+using Stormancer.Server.Plugins.Party;
 using Stormancer.Server.Plugins.Users;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Stormancer.Server.Plugins.Steam
 {
     /// <summary>
-    /// Steam controller.
+    /// Steam party controller
     /// </summary>
-    public class SteamController : ControllerBase
+    public class SteamPartyController : ControllerBase
     {
-        private IUserService _userService { get; set; }
+        private IPartyService _partyService { get; set; }
+        private IUserSessions _userSessions { get; set; }
         private ISteamService _steamService { get; set; }
 
         /// <summary>
-        /// Steam controller constructor.
+        /// Steam party controller constructor
         /// </summary>
-        /// <param name="userService"></param>
+        /// <param name="partyService"></param>
+        /// <param name="userSessions"></param>
         /// <param name="steamService"></param>
-        public SteamController(IUserService userService, ISteamService steamService)
+        public SteamPartyController(IPartyService partyService, IUserSessions userSessions, ISteamService steamService)
         {
-            _userService = userService;
+            _partyService = partyService;
+            _userSessions = userSessions;
             _steamService = steamService;
         }
 
         /// <summary>
-        /// Query stormancer user ids from steam ids.
+        /// Create lobby metadata bearer token.
         /// </summary>
-        /// <param name="steamIds"></param>
-        /// <returns>Map<steamId, userId></returns>
-        /// <remarks>Obsolete: This api has some security issues</remarks>
-        [Obsolete]
-        [Api(ApiAccess.Public, ApiType.Rpc)]
-        public async Task<Dictionary<ulong, string>> QueryUserIds(IEnumerable<ulong> steamIds)
-        {
-            var users = await _userService.GetUsersByClaim2(SteamConstants.PROVIDER_NAME, SteamConstants.ClaimPath, steamIds.Select(steamId => steamId.ToString()).ToArray());
-            return users.ToDictionary(kvp => ulong.Parse(kvp.Key), kvp => kvp.Value.Id);
-        }
-
-        /// <summary>
-        /// Decode Lobby metadata bearer token.
-        /// </summary>
-        /// <param name="tokens">Tokens to decode</param>
         /// <returns></returns>
         [Api(ApiAccess.Public, ApiType.Rpc)]
-        public async Task<IEnumerable<LobbyMetadataDto>> DecodePartyDataBearerTokens(IEnumerable<string> tokens)
+        public async Task<string> CreatePartyDataBearerToken()
         {
-            return await _steamService.DecodePartyDataBearerTokens(tokens);
+            var user = await _userSessions.GetUser(Request.RemotePeer);
+            var steamIdStr = (string?)user.Auth[SteamConstants.PROVIDER_NAME]?[SteamConstants.ClaimPath];
+            ulong steamId;
+
+            if (steamIdStr == null)
+            {
+                throw new Exception("SteamId is null");
+            }
+
+            try
+            {
+                steamId = ulong.Parse(steamIdStr);
+            }
+            catch (Exception)
+            {
+                throw new Exception("SteamId is invalid");
+            }
+
+            return await _steamService.CreatePartyDataBearerToken(steamId, user.Id, _partyService.PartyId);
         }
     }
 }
