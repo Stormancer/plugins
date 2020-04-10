d29b63d120a6f3a794ecacfe3a21db921ed670e0
Jean-Noël Gourdol <jngourdol@stormancer.com>
2020-04-10 13:33:30 +0200

    SpatialOS credentials creation

---
+++ src/Stormancer.Plugins/SpatialOS/Stormancer.Server.Plugins.SpatialOS/Models/SpatialOSConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stormancer.Server.Plugins.SpatialOS.Models
+{
+    /// <summary>
+    /// SpatialOS configuration for the Stormancer application
+    /// </summary>
+    /// <remarks>
+    /// Place this configuration in the configuration property "spatialos"
+    /// </remarks>
+    public class SpatialOSConfiguration
+    {
+        /// <summary>
+        /// The SpatialOS service key. 
+        /// </summary>
+        public string ServiceKey { get; set; } = "";
+
+        /// <summary>
+        /// The SpatialOS project name
+        /// </summary>
+        public string ProjectName { get; set; } = "";
+
+        /// <summary>
+        /// The login token validity durations, in minutes
+        /// </summary>
+        public int LoginTokenDuration { get; set; }
+
+        /// <summary>
+        /// the default SpatialOS deployment name for generating login token. Can be overridden by an event handler.
+        /// </summary>
+        public string? DefaultDeploymentName { get; set; }
+    }
+}
