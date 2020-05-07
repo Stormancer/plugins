4b9ed750c25f928c20d345aa5eb69498d10a7f51
Hector Labanca <hlabanca@stormancer.com>
2020-05-07 16:19:12 +0200

    ADD: Utilities: IEnumerable.WhereNotNull()

---
+++ src/Stormancer.Plugins/Utilities/Stormancer.Server.Plugins.Utilities/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stormancer.Server.Plugins.Utilities.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Filter out null values from a collection of nullables.
+        /// </summary>
+        /// <typeparam name="T">Type of the IEnumerable</typeparam>
+        /// <param name="source">IEnumerable to be filtered</param>
+        /// <returns>A copy of <paramref name="source"/> without null values.</returns>
+        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source)
+            where T : class
+        {
+            return source.Where(x => x != null)!;
+        }
+    }
+}
