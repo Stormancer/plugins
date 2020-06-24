89b9dea3738d9046186372cd2cdefa4a675de462
Jean-Michel Deruty <jmderuty@stormancer.com>
2020-06-24 16:36:33 +0200

    FIX use transformed leaderboardName when calling GetScore

--- src/Stormancer.Plugins/Leaderboards/Stormancer.Server.Plugins.Leaderboards/LeaderboardService.cs
+++ src/Stormancer.Plugins/Leaderboards/Stormancer.Server.Plugins.Leaderboards/LeaderboardService.cs
@@ -152,12 +152,12 @@ namespace Stormancer.Server.Plugins.Leaderboards
         {
             var index = GetModifiedLeaderboardName(leaderboardName);
             var client = await CreateESClient<ScoreRecord>(index);
-            var startResult = await client.GetAsync<ScoreRecord>(GetDocumentId(leaderboardName, playerId));
+            var startResult = await client.GetAsync<ScoreRecord>(GetDocumentId(index, playerId));
             if (!startResult.Found)
             {
                 return null;
             }
-            startResult.Source.Id = ParseDocumentId(startResult.Id).Item2;
+            
             return startResult.Source;
         }
 
@@ -379,8 +379,7 @@ namespace Stormancer.Server.Plugins.Leaderboards
                 foreach (var doc in documents.Take(leaderboardQuery.Count))
                 {
                     //Remove leaderboardName from document.
-                    var (_, id) = ParseDocumentId(doc.Id);
-                    doc.Id = id;
+                   
 
                     if (EnableExequo)
                     {
@@ -437,11 +436,7 @@ namespace Stormancer.Server.Plugins.Leaderboards
             return leaderboardResult;
         }
         
-        private static (string,string) ParseDocumentId(string documentId)
-        {
-            var segments = documentId.Split('#');
-            return (segments[0], segments[1]);
-        }
+
         private string SerializeContinuationQuery(LeaderboardContinuationQuery query)
         {
             var json = Newtonsoft.Json.JsonConvert.SerializeObject(query);
