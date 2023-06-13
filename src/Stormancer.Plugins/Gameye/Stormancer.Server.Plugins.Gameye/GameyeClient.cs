14edc9c6bd7c9820a17110ff98a2cf543356a3ae
Jean-Michel Deruty <nia.elfette@live.fr>
2023-06-13 15:50:48 +0200

    Add missing namespaces

--- src/Stormancer.Plugins/Gameye/Stormancer.Server.Plugins.Gameye/GameyeClient.cs
+++ src/Stormancer.Plugins/Gameye/Stormancer.Server.Plugins.Gameye/GameyeClient.cs
@@ -4,9 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Stormancer.Server.Plugins.Gameye
