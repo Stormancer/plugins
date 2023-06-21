519a2de8fffbca525c0c625b371d1b6036062f57
Jean-Michel Deruty <nia.elfette@live.fr>
2023-06-21 19:44:19 +0200

    don't translate the party service name to a sceneId when it starts with party. It's already a sceneId in this case.

--- src/Stormancer.Plugins/Party/Stormancer.Server.Plugins.Party/PartySceneLocator.cs
+++ src/Stormancer.Plugins/Party/Stormancer.Server.Plugins.Party/PartySceneLocator.cs
@@ -16,7 +16,22 @@ namespace Stormancer.Server.Plugins.Party
                     ctx.SceneId = PartyPlugin.PARTY_MANAGEMENT_SCENEID;
                     break;
                 case PartyPlugin.PARTY_SERVICEID:
-                    ctx.SceneId = "party-"+ctx.ServiceName;
+                    if (!string.IsNullOrEmpty(ctx.ServiceName) )
+                    {
+                        //On WJ2, There is a bug in the Client Switch plugin. It requests a scene token by providing the scene id as 
+                        //the service name instead of using the partyId (as it should). If the serviceName already starts with 'party-',
+                        //we know that it's not a party id, but the target sceneId and we shouldn't try to translate it.
+                        if (!ctx.ServiceName.StartsWith("party-"))
+                        {
+                            ctx.SceneId = "party-" + ctx.ServiceName;
+                        }
+                        else
+                        {
+                            ctx.SceneId = ctx.ServiceName;
+                        }
+                    }
+                    
+                  
                     break;
                 default:
                     break;
