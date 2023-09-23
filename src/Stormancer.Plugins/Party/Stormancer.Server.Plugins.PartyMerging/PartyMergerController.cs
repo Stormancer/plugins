24e66e56280e665a4e10c4aa2b3d1bc36acbd8b6
Jean-Michel Deruty <nia.elfette@live.fr>
2023-09-23 14:52:30 +0200

    Party merging initial commit

---
+++ src/Stormancer.Plugins/Party/Stormancer.Server.Plugins.PartyMerging/PartyMergerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stormancer.Server.Plugins.PartyMerging
+{
+    [Service(Named =true,ServiceType ="Stormancer.PartyMerger")]
+    internal class PartyMergerController
+    {
+        private readonly PartyMergingService _service;
+
+        public PartyMergerController(PartyMergingService service)
+        {
+            _service = service;
+        }
+
+        [S2SApi]
+        public  Task Merge(string partyId, CancellationToken cancellationToken)
+        {
+            return _service.StartMergeParty(partyId, cancellationToken);
+        }
+    }
+}
