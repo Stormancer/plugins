d27ed8271c4e06c996fcf5e1131cff54abb39e99
Jean-Michel Deruty <nia.elfette@live.fr>
2023-10-17 15:53:51 +0200

    fix compilation errors with plugins WIP

--- src/Stormancer.Plugins/Azure/Stormancer.Server.Plugins.Azure/App.cs
+++ src/Stormancer.Plugins/Azure/Stormancer.Server.Plugins.Azure/App.cs
@@ -1,10 +1,20 @@
-﻿namespace Stormancer.Server.Plugins.Azure
+﻿using Stormancer.Plugins;
+
+namespace Stormancer.Server.Plugins.Azure
 {
     public class App
     {
         public void Run(IAppBuilder builder)
         {
-            builder.AddPlugin(new EntityFrameworkCorePlugin());
+            builder.AddPlugin(new AzurePlugin());
+        }
+    }
+
+    internal class AzurePlugin : IHostPlugin
+    {
+        public void Build(HostPluginBuildContext ctx)
+        {
+           
         }
     }
 }
--- src/Stormancer.Plugins/Blobs/Stormancer.Server.Plugins.BlobStorage/App.cs
+++ src/Stormancer.Plugins/Blobs/Stormancer.Server.Plugins.BlobStorage/App.cs
@@ -1,10 +1,20 @@
-﻿namespace Stormancer.Server.Plugins.BlobStorage
+﻿using Stormancer.Plugins;
+
+namespace Stormancer.Server.Plugins.BlobStorage
 {
     public class App
     {
         public void Run(IAppBuilder builder)
         {
-            builder.AddPlugin(new EntityFrameworkCorePlugin());
+            builder.AddPlugin(new BlobStoragePlugin());
+        }
+    }
+
+    internal class BlobStoragePlugin : IHostPlugin
+    {
+        public void Build(HostPluginBuildContext ctx)
+        {
+            
         }
     }
 }
