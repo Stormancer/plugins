e92ab9bdabf6ec21b9663dd8798552eebb6b53f4
Jean-Michel Deruty <nia.elfette@live.fr>
2024-03-14 17:59:42 +0100

    Add sample app that can be used online

---
+++ tests/Stormancer.Server.Plugins.Tests.Sample/Program.cs
@@ -0,0 +1,13 @@
+﻿using Stormancer.Server.Hosting;
+using Stormancer.Server.Plugins.Tests.Sample;
+public class Program
+{
+    public static Task Main(string[] args)
+    {
+
+        return ServerApplication.Run(builder => builder
+           .Configure(args)
+           .AddAllStartupActions()
+        );
+    }
+}
