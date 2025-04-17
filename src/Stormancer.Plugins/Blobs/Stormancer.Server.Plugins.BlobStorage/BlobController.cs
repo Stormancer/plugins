41bc7c2018ba1cce3b45dbe4a29b234e17e31040
Jean-Michel Deruty <jmderuty@stormancer.com>
2025-04-17 16:51:40 +0200

    Fix BlobStorage build

--- src/Stormancer.Plugins/Blobs/Stormancer.Server.Plugins.BlobStorage/BlobController.cs
+++ src/Stormancer.Plugins/Blobs/Stormancer.Server.Plugins.BlobStorage/BlobController.cs
@@ -1,4 +1,5 @@
-﻿using Stormancer.Plugins;
+﻿using MessagePack;
+using Stormancer.Plugins;
 using Stormancer.Server.Plugins.API;
 using System.Buffers;
 using System.Collections.Generic;
@@ -34,50 +35,56 @@ namespace Stormancer.Server.Plugins.BlobStorage
             using var  owner = MemoryPool<byte>.Shared.Rent(length);
             var buffer = owner.Memory.Slice(0, length);
             request.InputStream.Read(buffer.Span);
-            await _blobStorage.StageBlockAsync(args.Token, args.BlockId, buffer,request.CancellationToken);
+            await _blobStorage.StageBlockAsync(request.RemotePeer.SessionId,args.Token, args.BlockId, buffer,request.CancellationToken);
         }
 
         /// <summary>
         /// Commits a blob from block ids.
         /// </summary>
         /// <param name="args"></param>
-        /// <param name="cancellationToken"></param>
+        /// <param name="request"></param>
         /// <returns></returns>
-        public async Task CommitBlob(CommitBlobArgs args,CancellationToken cancellationToken)
+        public async Task CommitBlocks(CommitBlobArgs args,RequestContext<IScenePeerClient> request)
         {
-            await _blobStorage.CommitBlockListAsync(args.Token, args.BlockIds,cancellationToken);
+            await _blobStorage.CommitBlockListAsync(request.RemotePeer.SessionId,args.Token, args.BlockIds,request.CancellationToken);
         }
     }
 
     /// <summary>
     /// Arguments for <see cref="BlobController.StageBlock(StageBlockArgs, RequestContext{IScenePeerClient})"/>
     /// </summary>
+    [MessagePackObject]
     public class StageBlockArgs
     {
         /// <summary>
         /// Gets or sets the token to use to authorize upload.
         /// </summary>
+        [Key(0)]
         public required string Token { get; init; }
 
         /// <summary>
         /// Gets or sets the id of the block to upload.
         /// </summary>
+        [Key(1)]
         public required string BlockId { get; init; }
     }
 
     /// <summary>
-    /// Arguments for <see cref="BlobController.CommitBlob(CommitBlobArgs, CancellationToken)"/>
+    /// Arguments for <see cref="BlobController.CommitBlocks(CommitBlobArgs, RequestContext{IScenePeerClient})"/>
     /// </summary>
+    [MessagePackObject]
     public class CommitBlobArgs
     {
         /// <summary>
         /// Gets or sets the token to use to authorize upload.
         /// </summary>
+        [Key(0)]
         public required string Token { get; init; }
 
         /// <summary>
         /// Gets the list of successive block id used to commit the blob.
         /// </summary>
+        [Key(1)]
         public required IEnumerable<string> BlockIds { get; init; }
         
     }
---
+++ src/Stormancer.Plugins/Blobs/cpp/blobStorage.hpp
@@ -0,0 +1,165 @@
+// MIT License
+//
+// Copyright (c) 2020 Stormancer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+#include "Users/ClientAPI.hpp"
+#include "stormancer/IPlugin.h"
+
+namespace Stormancer
+{
+	namespace BlobStorage
+	{
+		namespace details
+		{
+			struct StageBlockArgs
+			{
+				std::string token;
+				std::string blockId;
+
+				MSGPACK_DEFINE(token, blockId)
+			};
+
+			struct CommitBlocksArgs
+			{
+				std::string token;
+				std::vector<std::string> blockIds;
+
+				MSGPACK_DEFINE(token,blockIds)
+			};
+
+			class BlobStorageService
+			{
+			public:
+
+				BlobStorageService(std::weak_ptr<RpcService> rpc, std::shared_ptr<Serializer> serializer)
+					: _serializer(serializer)
+					, _rpc(rpc)
+				{
+				}
+
+			
+				pplx::task<void> stageBlock(std::string uploadToken, std::string blockId, const byte* buffer, const size_t length)
+				{
+					auto rpc = _rpc.lock();
+					auto serializer = _serializer;
+					return rpc->rpc("Blob.StageBlock", [serializer, uploadToken, blockId, buffer, length](obytestream& stream)
+					{
+						StageBlockArgs args;
+						args.token = uploadToken;
+						args.blockId = blockId;
+						serializer->serialize(stream, args);
+						stream.write(buffer, length);
+					});
+				}
+
+
+			
+				pplx::task<void> commitBlocks(std::string uploadToken, std::vector<std::string> blockIds)
+				{
+					auto rpc = _rpc.lock();
+					CommitBlocksArgs args;
+					args.token = uploadToken;
+					args.blockIds = blockIds;
+					return rpc->rpc("Blob.CommitBlocks",args);
+				}
+
+
+			private:
+
+				std::shared_ptr<Serializer> _serializer;
+				std::weak_ptr<RpcService> _rpc;
+			};
+		}
+
+		class BlobStoragePlugin;
+		class BlobStorageApi : public  ClientAPI<BlobStorageApi, details::BlobStorageService>
+		{
+			friend class ReportsPlugin;
+		public:
+
+			BlobStorageApi(std::weak_ptr<Users::UsersApi> users)
+				: ClientAPI(users, "stormancer.blobstorage")
+			{
+
+			}
+
+
+		
+			pplx::task<void> stageBlock(std::string uploadToken, std::string blockId, const byte* buffer, const size_t length)
+			{
+				if (length > 4 * 1024 * 1024)
+				{
+					return pplx::task_from_exception<void>(std::runtime_error("data cannot be more than 4MB"));
+				}
+
+				return getService().then([uploadToken, blockId, buffer, length](std::shared_ptr<details::BlobStorageService> service)
+				{
+					return service->stageBlock(uploadToken,blockId,buffer,length);
+				});
+			}
+
+		
+			pplx::task<void> commitBlocks(std::string uploadToken, std::vector<std::string> blockIds)
+			{
+				
+				return getService().then([uploadToken, blockIds](std::shared_ptr<details::BlobStorageService> service)
+				{
+					return service->commitBlocks(uploadToken, blockIds);
+				});
+			}
+
+
+		private:
+
+		};
+
+
+		class BlobStoragePlugin : public Stormancer::IPlugin
+		{
+			static constexpr const char* PLUGIN_NAME = "BlobStorage";
+			static constexpr const char* PLUGIN_VERSION = "1.0.0";
+			PluginDescription getDescription() override
+			{
+				return PluginDescription(PLUGIN_NAME, PLUGIN_VERSION);
+			}
+
+			void registerSceneDependencies(Stormancer::ContainerBuilder& builder, std::shared_ptr<Stormancer::Scene> scene) override
+			{
+
+				if (scene)
+				{
+					auto name = scene->getHostMetadata("stormancer.blobStorage");
+
+					if (!name.empty())
+					{
+						builder.registerDependency<Stormancer::BlobStorage::details::BlobStorageService, RpcService, Serializer>().singleInstance();
+					}
+				}
+
+			}
+			void registerClientDependencies(Stormancer::ContainerBuilder& builder) override
+			{
+				builder.registerDependency<Stormancer::BlobStorage::BlobStorageApi, Stormancer::Users::UsersApi>().as<Stormancer::Reports::ReportsApi>().singleInstance();
+			}
+		};
+	}
+}
