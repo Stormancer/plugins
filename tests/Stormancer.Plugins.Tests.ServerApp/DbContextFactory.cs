7c2dc9ae1d586fd1d773385d32929e515a1ccb00
Jean-Noël Gourdol <jngourdol@stormancer.com>
2025-07-21 11:16:52 +0200

    Adds Database provider to the sample

---
+++ tests/Stormancer.Plugins.Tests.ServerApp/DbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Stormancer.Server.Hosting;
+using Stormancer.Server.Plugins.Database.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stormancer.Plugins.Tests.ServerApp
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var host = ServerApplication.CreateDesignTimeHost(builder => builder.AddAllStartupActions());
+            var scope = host.DependencyResolver.CreateChild(Stormancer.Server.Plugins.API.Constants.ApiRequestTag);
+
+            return scope.Resolve<DbContextAccessor>().GetDbContextAsync().Result;
+        }
+    }
+}
