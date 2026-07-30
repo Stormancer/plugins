ce445f4109fd483cf8dfb7db4fd1c200252b5a64
Jean-Noël Gourdol <jngourdol@stormancer.com>
2026-07-30 18:33:52 +0200

    Dependency registrations

--- src/Stormancer.Plugins/Aws/Stormancer.Server.Plugins.Aws/App.cs
+++ src/Stormancer.Plugins/Aws/Stormancer.Server.Plugins.Aws/App.cs
@@ -2,6 +2,13 @@
 {
     public class App
     {
-
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="builder"></param>
+        public void Run(IAppBuilder builder)
+        {
+            builder.AddPlugin(new AwsPlugin());
+        }
     }
 }
--- src/Stormancer.Plugins/Aws/Stormancer.Server.Plugins.Aws/AwsPlugin.cs
+++ src/Stormancer.Plugins/Aws/Stormancer.Server.Plugins.Aws/AwsPlugin.cs
@@ -1,4 +1,7 @@
-﻿using Stormancer.Plugins;
+﻿using Stormancer.Diagnostics;
+using Stormancer.Plugins;
+using Stormancer.Server.Plugins.Aws.Config;
+using Stormancer.Server.Plugins.BlobStorage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,10 @@ namespace Stormancer.Server.Plugins.Aws
         {
             ctx.HostDependenciesRegistration += (IDependencyBuilder builder) =>
             {
-
+                builder.Register<ConfigCache>().SingleInstance();
+                builder.Register<S3BlobStorageBackend>(resolver =>
+                    new S3BlobStorageBackend(resolver.Resolve<ConfigCache>(), resolver.Resolve<ILogger>())
+               ).As<IBlobStorageBackend>().InstancePerRequest();
             };
         }
     }
